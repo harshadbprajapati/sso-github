5519692b3f19bd43c2507fa214a11266162243df
Harshad <harshad.b.prajapati@gmail.com>
2022-12-31 07:53:24 +0530

    Initial Commit

---
+++ src/test/java/com/example/ssogithub/SsoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
